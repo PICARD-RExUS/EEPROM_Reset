685351747c952216a432255cdd36ae5478e880e3
Prototype Inflatable Conical Antenna - RExUS Deployment <Piggzonline@users.noreply.github.com>
2015-09-22 16:59:07 +0100

    Create EEPROM_Reset.ino

---
+++ EEPROM_Reset.ino
@@ -0,0 +1,20 @@
+@@ -0,0 +1,19 @@
+#include <EEPROM.h>
+
+int addr = 0;
+byte x;
+
+void setup() {
+
+    Serial.begin(9600);
+  while (!Serial) {
+    ;  } }
+
+void loop() {
+
+  x = EEPROM.read(addr);
+//Serial.println(addr);
+Serial.println(x);
+x = 0;
+EEPROM.write(0,x);
+delay(1000); }
